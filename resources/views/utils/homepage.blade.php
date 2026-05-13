20db7a00e4cf8c3250d22281f0fb75c810524b5c
SaraBarrett <sara.c.mon@gmail.com>
2026-05-13 15:36:58 +0100

    blade vars

--- resources/views/utils/homepage.blade.php
+++ resources/views/utils/homepage.blade.php
@@ -1,24 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.fo')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-</head>
+@section('content')
+    @php
+        //consulta à base de dados
+        $class = 'SD';
 
-<body>
+        //consulta à bd select * from students
+        $students = ['Luís', 'Afonso', 'Eduarda', 'Joaquim'];
+    @endphp
+
+
     <h4>Bem vindo à aplicação de Servidor!</h4>
-    <img src="{{ asset('') }}" alt="">
+
+    @if ($class)
+        <h3>Olá turma {{ $class }}</h3>
+        <ul>
+            @foreach ($students as $item)
+                <li>{{ $item }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <img src="{{ asset('images/what-is-software-CA-Capterra-Header.png') }}" alt="">
+
     <ul>
         <li><a href="{{ route('welcome_routename') }}">Welcome</a></li>
         <li><a href="{{ route('testevariaveis') }}">Variáveis</a></li>
         <li><a href="{{ route('users.add') }}">Adicionar user</a></li>
     </ul>
-
-
-</body>
-
-</html>
+@endsection
